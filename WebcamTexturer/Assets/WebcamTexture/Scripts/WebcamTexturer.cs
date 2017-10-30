750b2fa97336a6a86a124825a742c3235ca8f8f9
kevinta893 <kevinta893@gmail.com>
2017-10-30 14:00:00 -0600

    Added debug and prefab

--- WebcamTexturer/Assets/WebcamTexture/Scripts/WebcamTexturer.cs
+++ WebcamTexturer/Assets/WebcamTexture/Scripts/WebcamTexturer.cs
@@ -8,12 +8,15 @@ public class WebcamTexturer : MonoBehaviour {
     [Header("Initialization")]
     [Tooltip("Lets you choose the camera you wish to use initially.")]
     public int startDeviceIndex = 0;
+    [Tooltip("Dumps out debug information into the log. Useful for Android/iOS device debugging")]
+    public bool dumpDebugInformation = false;
+
 
     [Header("Debug (read only)")]
-    public string cameraName;
+    public string activeCameraName;
     public Vector2 resolution;
     public string[] avaliableDevices;
-
+    public float aspectRatio = 0.0f;
 
     private WebCamTexture webcamTexture;
 
@@ -27,7 +30,7 @@ public class WebcamTexturer : MonoBehaviour {
         }
 
         //create the new webcam texture and start it
-        WebCamTexture webcamTexture = new WebCamTexture();
+        webcamTexture = new WebCamTexture();
         webcamTexture.deviceName = camDevices[startDeviceIndex].name;
         Renderer renderer = GetComponent<Renderer>();
         renderer.material.mainTexture = webcamTexture;
@@ -35,17 +38,28 @@ public class WebcamTexturer : MonoBehaviour {
 
 
         //display debug information
-        this.cameraName = webcamTexture.deviceName;
+        this.activeCameraName = webcamTexture.deviceName;
         this.resolution = new Vector2(webcamTexture.width, webcamTexture.height);
+        this.aspectRatio = (float) webcamTexture.width / (float) webcamTexture.height;
         this.avaliableDevices = new string[camDevices.Length];
         for(int i = 0; i < camDevices.Length; i++)
         {
             this.avaliableDevices[i] = camDevices[i].name;
         }
 
-        Debug.Log("Using camera= " + webcamTexture.deviceName + " (" + camDevices.Length + " total devices)");
-    }
 
+        Debug.Log("Webcam started, using camera= " + webcamTexture.deviceName + " (" + camDevices.Length + " total devices)");
+        
+        if (dumpDebugInformation)
+        {
+            Debug.Log(
+                "Camera name=" + this.activeCameraName + "\n" +
+                "Resolution=" + this.resolution.ToString() + "\n" +
+                "Aspect Ratio=" + this.aspectRatio + "\n" +
+                "Total Devices=" + this.avaliableDevices.Length + "\n"
+                );
+        }
+    }
 
 
     public void Play()
